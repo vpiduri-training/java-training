67db6e375cc4639eca2572ac24d5b3b174f41592
RobertZizo <Robertzizo@yahoo.com>
2018-03-13 01:01:20 -0700

    added BasicsDemoRobertZizo file

---
+++ BasicsDemoRobertZizo.java
@@ -0,0 +1,48 @@
+class BasicsDemoRobertZizo {
+  // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+  static void print() {
+    System.out.println("\n\nInside print..."); // \n for new line
+    System.out.println("Hello, world"); // Advance cursor to beginning of next line
+    System.out.print("Hello, world!!!!! ");   // Cursor stayed after the printed string
+    System.out.println("Hello,");
+    System.out.print("                  ");                // Print eighteen spaces
+    System.out.print("world!!");
+    System.out.println();                 // Print empty line
+    System.out.println();                 // Print empty line
+    System.out.println();                 // Print empty line
+    System.out.println();                 // Print empty line
+    System.out.println();                 // Print empty line
+    System.out.print("Hello...");
+    System.out.print(" ");                // Print a space
+    System.out.print("is it me you're looking for?");
+    System.out.println();                 // Print empty line
+    System.out.println("                             ;\\ \r\n" + 
+    		"                            |' \\ \r\n" + 
+    		"         _                  ; : ; \r\n" + 
+    		"        / `-.              /: : | \r\n" + 
+    		"       |  ,-.`-.          ,': : | \r\n" + 
+    		"       \\  :  `. `.       ,'-. : | \r\n" + 
+    		"        \\ ;    ;  `-.__,'    `-.| \r\n" + 
+    		"         \\ ;   ;  :::  ,::'`:.  `. \r\n" + 
+    		"          \\ `-. :  `    :.    `.  \\ \r\n" + 
+    		"           \\   \\    ,   ;   ,:    (\\ \r\n" + 
+    		"            \\   :., :.    ,'o ): ` `-. \r\n" + 
+    		"           ,/,' ;' ,::\"'`.`---'   `.  `-._ \r\n" + 
+    		"         ,/  :  ; '\"      `;'          ,--`. \r\n" + 
+    		"        ;/   :; ;             ,:'     (   ,:) \r\n" + 
+    		"          ,.,:.    ; ,:.,  ,-._ `.     \\\"\"'/ \r\n" + 
+    		"          '::'     `:'`  ,'(  \\`._____.-'\"' \r\n" + 
+    		"             ;,   ;  `.  `. `._`-.  \\\\ \r\n" + 
+    		"             ;:.  ;:       `-._`-.\\  \\`. \r\n" + 
+    		"              '`:. :        |' `. `\\  ) \\ \r\n" + 
+    		"                 ` ;:       |    `--\\__,' \r\n" + 
+    		"                   '`      ,' \r\n" + 
+    		"                        ,-' \r\n" + 
+    		"");
+  }
+  
+  public static void main(String[] args) {	
+	   	// Language Basics 1
+		print();				
+  }       
+}
