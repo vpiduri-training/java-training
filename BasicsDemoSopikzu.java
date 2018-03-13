efa0b6ddd272fc05563a0a9c7c0a24a268010a5f
Thomas <sopikz123@gmail.com>
2018-03-13 12:03:01 -0700

    Added demo file for user Sopikzu

---
+++ BasicsDemoSopikzu.java
@@ -0,0 +1,55 @@
+package com.training.java.basics;
+
+public class BasicsDemoSopikzu {
+
+	static void print() {
+		System.out.println("                    Here the bad Coding begins");
+		/*
+		 * Has 20 characters of spacing before print statement and prints to next line.
+		 */
+		System.out.println();
+		System.out.println("       Be ready to read some awful coding, Phani, and thank you in advance for everything!");
+		/* 7 characters of spacing before the print statement. Just for formatting */
+		System.out.println(); // Print empty line for spacing and formatting
+		System.out.println(
+				"     ------------------------------------------------------------------------------------------"); // Characters
+		/*
+		 * 5 characters of spacing before print statement, along with 90 characters of
+		 * dashes for formatting.
+		 */
+		System.out.print("  Here  " + "  Is  " + "  An  " + "  Example  ");
+		/*
+		 * Multiple print statement here, will print continously to one line only, each
+		 * segment having 2 characters of space before and after each word. Will not
+		 * return to next line, but will again continue to print to singular line.
+		 */
+		System.out.print("  Of  ");
+		// I started to combine multiple print commands to show flexibility within the
+		// code itself and how easy it was to manipulate.
+		System.out.print("  using multiple coding methods");
+		// This line was a continuation of the last line to combine multiple print
+		// commands into one line.
+		System.out.print(" to do very similar things. Watch how the next line is blank!");
+		// Continuation on same line.
+		System.out.println(); // Will advance characters to next line.
+		System.out.println(); // Prints out blank line.
+		System.out.print(
+				"All of this demonstrates how every different character in our code affects exactly how the machines process it and return the information."); //
+		System.out.println(); // Takes pointer to next line
+		System.out.println(); // Prints blank line
+		System.out.println(); // Prints blank line and returns to next
+		System.out.println(); // Prints blank line and advances pointer to next line.
+		System.out.println(); // Prints blank line and advances pointer to next line.
+		System.out.println(); // Prints blank line and advances pointer to next line.
+		System.out.println(); // Prints blank line and advances pointer to next line.
+		System.out.print(
+				"              One of my own major downfalls with coding is I feel I do not Document well enough.");
+		// 15 lines of Spacing before print statement.
+		System.out.println(); // Prints blank line and advances pointer to next line.
+	}
+
+	public static void main(String[] args) {
+		print(); // This line will invoke our newly made Print method
+	}
+
+}
