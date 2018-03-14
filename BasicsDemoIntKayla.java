11f71aba588117b9c2d25d5be3d4bd6b18312582
ikaylahall <37131088+ikaylahall@users.noreply.github.com>
2018-03-13 19:22:46 -0700

    Added user specific int basics demo

---
+++ BasicsDemoIntKayla.java
@@ -0,0 +1,23 @@
+	  class BasicsDemoInt {
+		    // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+		    }
+
+			static void primitives() {
+			  int intHex = 0x0043;
+			  System.out.println("intHex: " + intHex);
+			  int intBinary = 0b1000011;
+			  System.out.println("intBinary: " + intBinary);
+			  int intChar = 'C'; 
+			  System.out.println("intChar: " + intChar);
+			  int intUnderscore = 6_7;
+			  System.out.println("intUnderscore: " + intUnderscore);
+		    }
+
+		    public static void main(String[] args) {
+			  // Language Basics 1
+			  //print();
+			  primitives();
+		    }
+		  }
+
+
