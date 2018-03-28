c8a299e0a796ce1db5bc577885ad21be3be7764a
Phani Piduri <Phani.Piduri@aexp.com>
2018-03-28 13:07:26 -0700

    new files

---
+++ Excercise/StudentUtil.java
@@ -0,0 +1,15 @@
+public class StudentUtil {
+
+    public static double[] calculateGPA(int[] studentIdList, char[][] studentsGrades) {
+        // your code
+    }
+    
+    public static int[] getStudentsByGPA(double lower, double higher, int[] studentIdList, char[][] studentsGrades) {
+        // perform parameter validation. Return null if passed parameters are not valid
+        
+        // invoke calculateGPA
+        
+        // construct the result array and return it. You would need an extra for loop to compute the size of the resulting array
+    }
+    
+}
